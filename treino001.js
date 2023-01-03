40f699f669f2bd55cb9e536c46e721bdbe774abf
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2023-01-03 12:44:48 -0300

    Update treino001.js

--- treino001.js
+++ treino001.js
@@ -3,7 +3,7 @@ console.log(`A velocidade do seu carro é de ${vel}Km/h`)
 if (vel > 60) {
     console.log('Você ultrapassou a velociade permitida. MULTADO!')
 } 
-console.log('Dirija sempre usando cinto de segurança!') */
+console.log('Dirija sempre usando cinto de segurança!') 
 
 
 var país = 'Brasil'
@@ -12,4 +12,6 @@ if (país == 'Brasil') {
     console.log('Você é Brasileiro!')
 } else {
     console.log('Você é Estrangeiro!')
-}
+} */
+
+
