19e2e2eb6fd6375405cb3dc9e409526f951fdf7b
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2023-01-13 18:57:48 -0300

    atualização dos exercícios

--- modulo-e/ambiente.js
+++ modulo-e/ambiente.js
@@ -1,5 +1,13 @@
 var c = 1 
-while (c <= 3) {
-    console.log('tudo bem?')
+ do {                               
+    console.log(`passo ${c}`)      //Executa primeiro depois testa
     c++    //c = c + 1
-}
+} while (c <= 3)
+
+
+
+/* var c = 1 
+while (c <= 3) {
+    console.log('tudo bem?')        //testa primeiro depois executa
+    c++    //c = c + 1
+} */
