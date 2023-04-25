a3d3458906ac5115bfd87b7bfd334a1825dc14ff
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2023-04-25 16:04:16 -0300

    app game - definindo dimensão do palco

---
+++ app_mata_mosca/jogo.js
@@ -0,0 +1,10 @@
+var altura = 0
+var largura = 0
+
+function ajusteTamanhoPalco() {
+    altura = window.innerHeight
+    largura = window.innerWidth
+
+    console.log(altura, largura)
+}
+ajusteTamanhoPalco()
