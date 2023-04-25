5b344da5a3d9bd5c01ffd47475a0fc6c9899af78
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2023-04-25 17:18:51 -0300

    app game - criando posições randômicas

--- app_mata_mosca/jogo.js
+++ app_mata_mosca/jogo.js
@@ -5,6 +5,27 @@ function ajusteTamanhoPalco() {
     altura = window.innerHeight
     largura = window.innerWidth
 
-    console.log(altura, largura)
+    //console.log(altura, largura)
 }
 ajusteTamanhoPalco()
+
+function posicaoRandomica() {
+
+    var posicaoX = Math.floor(Math.random() * largura) - 90  //calcula o valor do palco do jogo definido anteriormente gerando valores aleatorios para que as moscas não vazem da tela do usuario
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+    
+    console.log(posicaoX, posicaoY)  //para fazer debug se tudo esta funcionando corretamente
+
+    //criar o elemento html
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosca.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+}
