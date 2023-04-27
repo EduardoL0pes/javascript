d05d8931e438afa11ccc3a0793a15bba458246f2
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2023-04-27 18:01:27 -0300

    app game - tamanhos randomicos

--- app_mata_mosca/jogo.js
+++ app_mata_mosca/jogo.js
@@ -10,7 +10,6 @@ function ajusteTamanhoPalco() {
 ajusteTamanhoPalco()
 
 function posicaoRandomica() {
-
     var posicaoX = Math.floor(Math.random() * largura) - 90  //calcula o valor do palco do jogo definido anteriormente gerando valores aleatorios para que as moscas não vazem da tela do usuario
     var posicaoY = Math.floor(Math.random() * altura) - 90
 
@@ -22,10 +21,26 @@ function posicaoRandomica() {
     //criar o elemento html
     var mosquito = document.createElement('img')
     mosquito.src = 'imagens/mosca.png'
-    mosquito.className = 'mosquito1'
+    mosquito.className = tamanhoAleatorio()  //alteração do atributo 'mosquito1' para chamada da função tamanhoAleatorio no **passo3**
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
 
     document.body.appendChild(mosquito)
+}
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3) //random - cria numeros aleatorios de 0 ate +/- 3   //floor - retira a fração do numero tornando inteiro
+    
+    switch(classe) {
+
+        case 0: 
+            return('mosquito1')
+
+        case 1: 
+            return('mosquito2')
+
+        case 2:
+            return('mosquito3')
+    }
 }
