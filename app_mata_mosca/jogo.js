99dbe7b2482f7bfe7190498bdcf6ad0102a0a40b
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2023-05-04 16:32:29 -0300

    app game - adicionando cronometro

--- app_mata_mosca/jogo.js
+++ app_mata_mosca/jogo.js
@@ -1,6 +1,7 @@
 var altura = 0
 var largura = 0
 var vidas = 1
+var tempo = 10
 
 function ajusteTamanhoPalco() {
     altura = window.innerHeight
@@ -9,6 +10,20 @@ function ajusteTamanhoPalco() {
     //console.log(altura, largura)
 }
 ajusteTamanhoPalco()
+    
+    var cronometro = setInterval(function(){
+        tempo -= 1
+
+        if(tempo < 0) {
+            clearInterval(cronometro)
+            clearInterval(criaMosquito)
+            alert('Vitória')
+
+        } else {
+            document.getElementById('cronometro').innerHTML = tempo 
+        }
+
+    }, 1000)
 
 function posicaoRandomica() {
 
