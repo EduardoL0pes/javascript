b64064f6047908988c9d6d3d56d0048eef95e751
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2023-07-06 18:04:06 -0300

    criando modal via JS

--- app_orcamento_pessoal/app.js
+++ app_orcamento_pessoal/app.js
@@ -61,13 +61,23 @@ function cadastrarDespesas() {
         valor.value)
     
     if(despesa.validarDados()) {
+        //bd.gravar(despesa)
+        document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
+        document.getElementById('modal_titulo_div').className = 'modal-header text-success'
+        document.getElementById('modal_conteudo').innerHTML = 'Despesa cadastrada com sucesso!'
+        document.getElementById('btn_modal').innerHTML = 'Voltar'
+        document.getElementById('btn_modal').className = 'btn btn-success'
+
         //dialog de sucesso
-        bd.gravar(despesa)
-        $('#sucessoGravacao').modal('show')
-        console.log('Dados válidos')
+        $('#modalRegistrarDespesa').modal('show')
     } else {
+        document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do registro'
+        document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
+        document.getElementById('modal_conteudo').innerHTML = 'Existem campos obrigatórios que não foram preenchidos, por favor verifique os dados!'
+        document.getElementById('btn_modal').innerHTML = 'Voltar e corrigir'
+        document.getElementById('btn_modal').className = 'btn btn-danger'
+
         //dialog de erro
-        $('#erroGravacao').modal('show')
-    }
-    
+        $('#modalRegistrarDespesa').modal('show')
+    }   
 }
