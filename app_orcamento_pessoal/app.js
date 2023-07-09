e9bf91d5d9c3c767e70411a501944902a955b13f
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2023-07-09 14:36:43 -0300

    adicionando tabela de despesa

--- app_orcamento_pessoal/app.js
+++ app_orcamento_pessoal/app.js
@@ -85,7 +85,7 @@ function cadastrarDespesas() {
         valor.value)
     
     if(despesa.validarDados()) {
-        //bd.gravar(despesa)
+        bd.gravar(despesa)
         document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
         document.getElementById('modal_titulo_div').className = 'modal-header text-success'
         document.getElementById('modal_conteudo').innerHTML = 'Despesa cadastrada com sucesso!'
@@ -108,9 +108,39 @@ function cadastrarDespesas() {
 
 function carregaListaDespesas() {
 
-    let despesa = Array()
+    let despesas = Array()
 
-    despesa = bd.recuperarTodosRegistros()
+    despesas = bd.recuperarTodosRegistros()
     
-    console.log(despesa)
+    //selecionando o elemento tbody da tabela
+    let listaDespesas = document.getElementById('listaDespesas')
+
+    //percorrer o array despesas, listando cada despesa de forma dinâmica
+    despesas.forEach(function(d) {
+        //console.log(d)
+        
+        //criando a linha(tr) 
+        let linha = listaDespesas.insertRow()
+        
+        //criar as colunas(td)
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+
+        //ajustar o tipo
+        switch(d.tipo) {
+            case '1': d.tipo = 'Alimentação'
+                break
+            case '2': d.tipo = 'Educação'
+                break
+            case '3': d.tipo = 'Lazer'
+                break
+            case '4': d.tipo = 'Saúde'
+                break
+            case '5': d.tipo = 'Transporte'
+                break
+        }
+        linha.insertCell(1).innerHTML = d.tipo
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = d.valor
+    })
 }
+
